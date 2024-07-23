7530ed9a7840815ba80baac7870787634a609f01
brenn-08 <anterobhea@gmail.com>
2024-07-23 17:16:33 +0800

    Added folder to repository

--- DemoFiles/sampleprogram.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem Open five websites
-start chrome "https://www.youtube.com/"
-start chrome "https://www.canva.com/"
-start chrome "https://www.britannica.com/"
-start chrome "https://www.grammarly.com/"
-start chrome "https://www.reddit.com/?rdt=48305"
